a49cfb3f426b19ba901ef4c01a7d4040b1b38b2a
8kyu <guillemsenabre@gmail.com>
2024-03-18 19:19:52 +0100

    header added ?

---
+++ Frontend/wlearning/app/components/header.tsx
@@ -0,0 +1,11 @@
+import styles from "../../styles/page.module.css"
+
+const MainHeader: React.FC = () => {
+  return (
+    <header className={styles.mainHeader}>
+      hola
+    </header>
+  );
+}
+
+export default MainHeader;
--- Frontend/wlearning/app/page.tsx
+++ Frontend/wlearning/app/page.tsx
@@ -1,9 +1,11 @@
 import styles from "../styles/page.module.css"
 import MainBodyContainer from "./containers/mainBodyContainer";
+import MainHeader from "./components/header";
 
 export default function Home() {
   return (
     <main className={styles.main}>
+      <MainHeader/>
       <MainBodyContainer/>
     </main>
   );
