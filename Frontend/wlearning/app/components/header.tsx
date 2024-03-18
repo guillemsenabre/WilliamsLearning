c522cfe83df81a3f529fca83041407b67f05c676
8kyu <guillemsenabre@gmail.com>
2024-03-18 19:33:13 +0100

    header: main structure ok?

--- Frontend/wlearning/app/components/header.tsx
+++ Frontend/wlearning/app/components/header.tsx
@@ -3,7 +3,9 @@ import styles from "../../styles/page.module.css"
 const MainHeader: React.FC = () => {
   return (
     <header className={styles.mainHeader}>
-      hola
+      <h3>WilliamS Learning</h3>
+      <h3>Search Bar</h3>
+      <h3>Log In button</h3>
     </header>
   );
 }
