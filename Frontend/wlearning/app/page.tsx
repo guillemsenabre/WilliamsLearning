92371ddbaafa98e56f3caccf1f73a65eaef3e44a
8kyu <guillemsenabre@gmail.com>
2024-03-18 18:45:49 +0100

    import: layouts/ -> containers/

--- Frontend/wlearning/app/page.tsx
+++ Frontend/wlearning/app/page.tsx
@@ -1,5 +1,5 @@
 import styles from "../styles/page.module.css"
-import MainBodyContainer from "./layouts/mainBodyContainer";
+import MainBodyContainer from "./containers/mainBodyContainer";
 
 export default function Home() {
   return (
