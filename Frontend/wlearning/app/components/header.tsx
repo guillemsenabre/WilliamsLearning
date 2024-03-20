f81b3d5b3f2434aabc3cccf58bf308d87574c336
8kyu <guillemsenabre@gmail.com>
2024-03-20 14:08:23 +0100

    feat: search bar ok. needed to transform server component to client component

--- Frontend/wlearning/app/components/header.tsx
+++ Frontend/wlearning/app/components/header.tsx
@@ -1,10 +1,11 @@
 import styles from "@/styles/page.module.css"
+import SearchBar from "./searchBar";
 
 const MainHeader: React.FC = () => {
   return (
     <header className={styles.mainHeader}>
-      <h3>WilliamS Learning</h3>
-      <h3>Search Bar</h3>
+      <h3>WilliamS Learning</h3>  
+      <SearchBar/>
       <h3>Log In button</h3>
     </header>
   );
--- Frontend/wlearning/app/components/searchBar.tsx
+++ Frontend/wlearning/app/components/searchBar.tsx
@@ -1,2 +1,21 @@
 import styles from "@/styles/page.module.css"
 
+'use client';
+const SearchBar: React.FC = () => {
+  function handleSearch(term: string) {
+    console.log(term);
+  }
+
+  return (
+    <div>
+      <input
+        placeholder="Search..."
+        onChange={(e) => {
+          handleSearch(e.target.value);
+        }}
+      />
+    </div>
+  );
+}
+
+export default SearchBar;
