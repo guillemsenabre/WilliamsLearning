97f5b0d3513130882ee6e2dff2b58ae644113091
8kyu <guillemsenabre@gmail.com>
2024-03-21 11:17:18 +0100

    module: getStatiProps moved to its own file

---
+++ Frontend/wlearning/app/scripts/getStaticProps.tsx
@@ -0,0 +1,17 @@
+// This function will run on the server-side before
+//rendering the page. 
+// Benefits:
+// -> Server-side rendering (faster initial load times, specially for big datasets)
+// -> Automatic parsing (although it's quite easy to do it manually)
+// -> Caching: nextjs can cache this data (improves performance when displaying it on other views)
+
+export async function getStaticProps() {
+  const response = await fetch('/app/data/cardData.json') // Retrieve the data from the json file
+  const cardData = await response.json() // parse the json file
+
+  return {
+    props: {
+      cardData,
+    },
+  };
+}
