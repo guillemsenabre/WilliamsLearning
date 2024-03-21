ebaadd55782ec281814bad6ffaacd596ed858985
8kyu <guillemsenabre@gmail.com>
2024-03-21 13:33:32 +0100

    feat: basic drop list ok

--- Frontend/wlearning/app/components/searchBar.tsx
+++ Frontend/wlearning/app/components/searchBar.tsx
@@ -1,23 +1,55 @@
 // Client Components, allows the use of handlers and hooks
 'use client';
 
+import { useState } from 'react'
+
 import styles from "@/styles/page.module.css"
 import cardData from "@/app/data/cardData.json"
 
+// Tsx Blueprint for cardData
+interface CardData {
+  field: string,
+  description: string, 
+  link?: string
+}
+
 const SearchBar: React.FC = () => {
+  /*
   function handleSearch(term: string) {
     console.log(term);
   }
+  */
+
+  const [searchTerm, setSearchTerm] = useState("");
+  const [showHints, setShowHints] = useState(false);
+
+  function handleSearchTerm(term: string) {
+    setSearchTerm(term);
+    setShowHints(true);
+  }
+
+  const filteredFields = cardData.filter((card) =>
+    card.field.toLowerCase().startsWith(searchTerm.toLowerCase())
+  );
 
   return (
     <div>
       <input
         className={styles.searchBar}
-        placeholder="Search..."
+        placeholder=""
         onChange={(e) => {
-          handleSearch(e.target.value);
+          handleSearchTerm(e.target.value);
         }}
       />
+      {showHints && filteredFields.length > 0 && (
+        <select onChange={() => console.log(setSearchTerm)}>
+          {filteredFields.map((card) => (
+            <option key={card.field} value={card.field}>
+              {card.field}
+            </option>
+          ))}
+        </select>
+      )}
     </div>
   );
 }
