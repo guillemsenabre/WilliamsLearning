5ebe9553562b4eb65ad0ff77d552d53258eb6ddb
8kyu <guillemsenabre@gmail.com>
2024-03-23 11:31:28 +0100

    struct: creating new container for general field page view

---
+++ Frontend/wlearning/app/containers/fieldContainer.tsx
@@ -0,0 +1,11 @@
+import styles from "@/styles/page.module.css"
+
+const FieldContainer = () => {
+  return (
+    <div>
+      <h1>Field N Container</h1>
+    </div>
+  );
+}
+
+export default FieldContainer;
--- Frontend/wlearning/app/mathematics/page.tsx
+++ Frontend/wlearning/app/mathematics/page.tsx
@@ -1,9 +1,11 @@
 import MainHeader from "@/app/components/header";
+import FieldContainer from "@/app/containers/fieldContainer";
 
 const Maths: React.FC = () => {
   return (
     <main>
       <MainHeader/>
+      <FieldContainer/>
     </main>
   );
 }
