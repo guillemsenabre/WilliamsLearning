04d3265a9f8af8acf83c2eff93170544287b8692
8kyu <guillemsenabre@gmail.com>
2024-03-25 10:39:49 +0100

    css: grid layout applied to parent div

--- Frontend/wlearning/app/containers/fieldContainer.tsx
+++ Frontend/wlearning/app/containers/fieldContainer.tsx
@@ -3,9 +3,7 @@ import styles from "@/styles/page.module.css"
 const FieldContainer = () => {
   return (
     <div className={styles.fieldContainer}>
-      <div>
-        <h1>Nav list</h1>
-      </div>
+      <div><h1>Nav list</h1></div>
       <div>Information</div>
     </div>
   );
