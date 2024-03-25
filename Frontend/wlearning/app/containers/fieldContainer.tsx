ca7c4f6fe160f89b1647f2c7350183b351e91f68
8kyu <guillemsenabre@gmail.com>
2024-03-25 10:52:26 +0100

    css: center text and other basic styles

--- Frontend/wlearning/app/containers/fieldContainer.tsx
+++ Frontend/wlearning/app/containers/fieldContainer.tsx
@@ -3,8 +3,24 @@ import styles from "@/styles/page.module.css"
 const FieldContainer = () => {
   return (
     <div className={styles.fieldContainer}>
-      <div><h1>Nav list</h1></div>
-      <div>Information</div>
+      <div className={styles.fieldMenu}>
+        <h1>Nav list</h1>
+        <ul>
+          <li><h2>Title</h2></li>
+          <li><h3>SubTitle</h3></li>
+          <li><h3>SubTitle</h3></li>
+          <li><h3>SubTitle</h3></li>
+          <li><h3>SubTitle</h3></li>
+          <li><h2>Title</h2></li>
+          <li><h3>SubTitle</h3></li>
+          <li><h3>SubTitle</h3></li>
+          <li><h3>SubTitle</h3></li>
+          <li><h3>SubTitle</h3></li>
+        </ul>
+      </div>
+      <div className={styles.fieldMain}>
+        <h1>Information</h1>
+      </div>
     </div>
   );
 }
