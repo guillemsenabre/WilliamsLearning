e4cb9f072fe754b0307f56ff2131dafa3b9e3f22
8kyu <guillemsenabre@gmail.com>
2024-03-25 10:33:57 +0100

    new component: field container, some divs

--- Frontend/wlearning/app/containers/fieldContainer.tsx
+++ Frontend/wlearning/app/containers/fieldContainer.tsx
@@ -2,8 +2,11 @@ import styles from "@/styles/page.module.css"
 
 const FieldContainer = () => {
   return (
-    <div>
-      <h1>Field N Container</h1>
+    <div className={styles.fieldContainer}>
+      <div>
+        <h1>Nav list</h1>
+      </div>
+      <div>Information</div>
     </div>
   );
 }
