002156b9488d9bdbc6dc83fe506d7d76236fde53
8kyu <guillemsenabre@gmail.com>
2024-03-25 20:37:57 +0100

    docs: comments on query properties

--- Frontend/wlearning/app/mathematics/page.tsx
+++ Frontend/wlearning/app/mathematics/page.tsx
@@ -7,9 +7,13 @@ import cardData from "@/app/data/cardData.json"
 
 const Maths: React.FC = () => {
 
+  // getParams is old router.query
   const searchParams = useSearchParams()
+
+  // Get query property "index"
   const index = searchParams.get('index')
 
+  // search field that matches index n
   const mathData = cardData.find(item => item.index == index);
 
   return (
