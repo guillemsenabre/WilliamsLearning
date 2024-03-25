4b712c5c8c751e5b75e2380184c88e79a0804554
8kyu <guillemsenabre@gmail.com>
2024-03-25 12:41:14 +0100

    refactor: card data now accepts index prop

--- Frontend/wlearning/app/containers/card.tsx
+++ Frontend/wlearning/app/containers/card.tsx
@@ -2,15 +2,16 @@ import styles from "@/styles/page.module.css"
 import Link from 'next/link'
 
 interface CardProps {
+  index: number;
   field: string;
   description: string;
   link: string;
 }
 
-const Card: React.FC<CardProps> = ({ field, description, link}) => {
+const Card: React.FC<CardProps> = ({ index, field, description, link}) => {
   return (
     <div className={styles.card}>
-      <Link href={ link }>
+      <Link href={ `${ link }?index=${index}` }>  
         <h2>
           { field } <span>-&gt;</span>
         </h2>
