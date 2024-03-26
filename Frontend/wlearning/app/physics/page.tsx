b61b356ef131ca30fbaba96b260822dd9a72c369
8kyu <guillemsenabre@gmail.com>
2024-03-26 12:11:38 +0100

    test: physics page created, module?

---
+++ Frontend/wlearning/app/physics/page.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+import MainHeader from "@/app/components/header";
+import FieldContainer from "@/app/containers/fieldContainer";
+import { useSearchParams } from "next/navigation"
+import cardData from "@/app/data/cardData.json"
+
+interface FieldData {
+  index?: string;
+  field: string;
+  description: string;
+  link?: string;
+}
+
+const Physics: React.FC = () => {
+
+  // getParams is old router.query
+  const searchParams = useSearchParams()
+
+  // Get query property "index" using non-null assertion operator "!".
+  const index: string = searchParams.get('index')!;
+
+  // Search field that matches index n. "as FieldData" is used to cast the FieldData type
+  const physicsData = cardData?.find(item => item.index == index) as FieldData;
+
+  // Using optional chaining to retrieve data from mathData as it may be undefined (unlikely)
+  const field = physicsData?.field;
+  const description = physicsData?.description;
+
+  return (
+    <main>
+      <MainHeader/>
+      <FieldContainer field={field} description={description} />
+    </main>
+  );
+}
+
+export default Physics;
