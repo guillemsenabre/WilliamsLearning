ff5ba822e938e61cde82fd07d4835b3ed8cb0ec1
8kyu <guillemsenabre@gmail.com>
2024-04-03 11:57:24 +0200

    refactor: getProperties updated with correct types

--- Frontend/wlearning/app/scripts/getProperties.tsx
+++ Frontend/wlearning/app/scripts/getProperties.tsx
@@ -1,23 +1,12 @@
-'use client';
+import { CardData } from "@/types/interfaces";
 
-import { useSearchParams } from "next/navigation";
-import cardData from "@/app/data/cardData.json"
-
-interface FieldData {
-  index?: string;
-  field: string;
-  description: string;
-  link?: string;
+interface Props {
+  data: CardData[];
+  index: string;
 }
 
-
-export function getProperties() {
-  // getParams is old router.query. Used to get index property
-  const searchParams = useSearchParams();
-
-  // Get query property "index" using non-null assertion operator "!".
-  const index: string = searchParams.get('index')!;
+export function getProperties({ data, index }: Props){
 
   // Search field that matches index n. "as FieldData" is used to cast the FieldData type
-  return  cardData?.find(item => item.index == index) as FieldData
+  return  data?.find(item => item.id == index) as CardData;
 }
