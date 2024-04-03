327dee6cf62fba09ff23113bed86ce2b4fd60218
8kyu <guillemsenabre@gmail.com>
2024-04-03 11:09:53 +0200

    refactor: mathematics page now gets data from backend as a server component

--- Frontend/wlearning/app/mathematics/page.tsx
+++ Frontend/wlearning/app/mathematics/page.tsx
@@ -1,11 +1,21 @@
 import MainHeader from "@/app/components/header";
 import FieldContainer from "@/app/containers/fieldContainer";
 
-const Maths: React.FC = () => {
+// Get data from sqlite3
+import { getCardData } from "@/libr/cardData";
+
+// Interface types & data structure
+import { CardData } from "@/types/interfaces";
+
+
+const Maths: React.FC<CardData> = () => {
+
+  const data: CardData[] = getCardData();
+  
   return (
     <main>
-      <MainHeader/>
-      <FieldContainer />
+      <MainHeader data = { data }/>
+      <FieldContainer data = { data }/>
     </main>
   );
 }
