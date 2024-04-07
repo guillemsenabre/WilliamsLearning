055ebb1ed45df48996d45d52474a00f56e99bfb8
8kyu <guillemsenabre@gmail.com>
2024-04-07 19:21:26 +0200

    feat: more thresholds have been set, bionic text improved and ok

--- nextApp/wlearning/app/scripts/bionicText.ts
+++ nextApp/wlearning/app/scripts/bionicText.ts
@@ -1,11 +1,25 @@
 function BionicText(text:string) {
+
   // Split the text into words
   const words = text.split(/\s+/); // Split on whitespace (including spaces, tabs, etc.)
 
   // Mapping over the words to add bold if length(word) > 3
   return words.map((word) => {
-    if (word.length > 3) {
+
+    // word length
+    const wlen: number = word.length;
+
+    // Bold limiters
+    const threshold_3: number = 3;
+    const threshold_5: number = 5;
+    const threshold_7: number = 7;
+
+    if (wlen > threshold_3 && wlen <= threshold_5) {
       return `<b>${word.slice(0,2)}</b>${word.slice(2)}`;
+    } else if (wlen > threshold_5 && wlen <= threshold_7) {
+      return `<b>${word.slice(0,3)}</b>${word.slice(3)}`;
+    } else if (wlen > threshold_7) {
+      return `<b>${word.slice(0,4)}</b>${word.slice(4)}`;
     } else {
       return word;
     }
