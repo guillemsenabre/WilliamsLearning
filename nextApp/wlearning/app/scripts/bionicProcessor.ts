976826283d54b2f72f8190d9cf6aa07eed89314e
8kyu <guillemsenabre@gmail.com>
2024-04-08 11:13:10 +0200

    feat: bionic text functions modularized

--- nextApp/wlearning/app/scripts/bionicProcessor.ts
+++ nextApp/wlearning/app/scripts/bionicProcessor.ts
@@ -1,5 +1,24 @@
-function BionicProcessor() {
+function BionicProcessor(paragraph: string) {
+  
+  // Split on whitespace (including spaces, tabs, etc.) word length
+  const processedWords: string[] = paragraph.split(/\s+/).map((word) => {
+    const wlen: number = word.length;
 
+    // NOTE - Could be converted to a math series function to improve O()
+    if (wlen > 1 && wlen <= 3) {
+      return `<b>${word.slice(0,1)}</b>${word.slice(1)}`;
+    } else if (wlen > 3 && wlen <= 5) {
+      return `<b>${word.slice(0,2)}</b>${word.slice(2)}`;
+    } else if (wlen > 5 && wlen <= 7) {
+      return `<b>${word.slice(0,3)}</b>${word.slice(3)}`;
+    } else if (wlen > 7) {
+      return `<b>${word.slice(0,4)}</b>${word.slice(4)}`;
+    } else {
+      return word;
+    }
+  });
+
+  return processedWords;
 }
 
 export default BionicProcessor;
--- nextApp/wlearning/app/scripts/bionicText.ts
+++ nextApp/wlearning/app/scripts/bionicText.ts
@@ -1,3 +1,6 @@
+import BionicProcessor from "./bionicProcessor";
+
+
 function BionicText(text:string) {
 
   // Split the text into paragraphs
@@ -10,23 +13,9 @@ function BionicText(text:string) {
   //styles if length(word) > 3
   paragraphs.forEach((paragraph) => {
 
-    // Split on whitespace (including spaces, tabs, etc.) word length
-    const processedWords = paragraph.split(/\s+/).map((word) => {
-      const wlen: number = word.length;
-
-      // NOTE - Could be converted to a math series function to improve O()
-      if (wlen > 1 && wlen <= 3) {
-        return `<b>${word.slice(0,1)}</b>${word.slice(1)}`;
-      } else if (wlen > 3 && wlen <= 5) {
-        return `<b>${word.slice(0,2)}</b>${word.slice(2)}`;
-      } else if (wlen > 5 && wlen <= 7) {
-        return `<b>${word.slice(0,3)}</b>${word.slice(3)}`;
-      } else if (wlen > 7) {
-        return `<b>${word.slice(0,4)}</b>${word.slice(4)}`;
-      } else {
-        return word;
-      }
-    });
+    // Bionic Processor splits paragraphs into words and
+    //process them
+    const processedWords: string[] = BionicProcessor(paragraph);
 
     // Join the processed words back into a paragraph
     processedParagraphs.push(processedWords.join(' '));
