8990845c69b5da6c301eb4c6011d889b747b6b59
8kyu <guillemsenabre@gmail.com>
2024-04-09 14:06:28 +0200

    data: improving database 1

--- nextApp/wlearning/libr/cardData.ts
+++ nextApp/wlearning/libr/cardData.ts
@@ -5,7 +5,18 @@ import { CardData } from "@/types/interfaces";
 
 const db = sql('data.db');
 
-export function getCardData() {
-  const data = db.prepare('SELECT * FROM data').all() as CardData[];
+export function getCardData(filter?: string): CardData[] {
+
+  // Select ALL rows in the database
+  let sql = `SELECT * FROM data`
+
+  // If a filter is provided, select only the row that matches it
+  if (filter) {
+    sql += `WHERE slug = ?`;
+  }
+
+  // sql will place `filter` instead of `?` as it is the placeholder
+  const data = db.prepare(sql).all(filter) as CardData[];
+
   return data;
 }
