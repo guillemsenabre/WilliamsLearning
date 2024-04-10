58e6e55a6a0764896750e4bbd4097bf3eef5bea3
8kyu <guillemsenabre@gmail.com>
2024-04-10 11:08:35 +0200

    docs: comment on slug identifier on mathematics/page.tsx

--- nextApp/wlearning/app/mathematics/page.tsx
+++ nextApp/wlearning/app/mathematics/page.tsx
@@ -13,13 +13,13 @@ import { CardData } from "@/types/interfaces";
 
 const Maths: React.FC<CardData> = () => {
 
+  // identifier to extract mathematics data from sqlite3 database
   const slug: string | undefined = '/mathematics';
 
   // Here we use type casting since we are sure that `getCardData()` will
   //return an array if no filter (slug) is provided and an object if it is.
   const data = getCardData() as CardData[];
   const mathData = getCardData(slug) as CardData;
-  //const mathData : CardData = getProperties({ data, index});
 
   return (
     <>
