5253c30c390ba58261f0d973e085243c460363f6
8kyu <guillemsenabre@gmail.com>
2024-04-11 10:51:17 +0200

    css: subfields grid ok

--- nextApp/wlearning/app/containers/fieldContainer.tsx
+++ nextApp/wlearning/app/containers/fieldContainer.tsx
@@ -62,7 +62,9 @@ const FieldContainer: React.FC<CardData> = ({ field, description, field_mindmap_
         <h2 id="subFields"> Sub fields </h2>
         <div className={styles.grid}>
           {subFieldsArray.map((subField) => (
-            <Card field = {subField}/>
+            <div>
+              <Card field = {subField}/>
+            </div>
           ))}
         </div>
       </div>
