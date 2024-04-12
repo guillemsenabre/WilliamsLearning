a1f9fc05b17aa8b61e345e9be25550409d7e11dd
8kyu <guillemsenabre@gmail.com>
2024-04-12 11:12:33 +0200

    css: footer basic styles

--- nextApp/wlearning/app/components/pageFooter.tsx
+++ nextApp/wlearning/app/components/pageFooter.tsx
@@ -3,8 +3,13 @@ import styles from "@/styles/page.module.css"
 
 const PageFooter: React.FC = () => {
   return (
-    <div className={styles.pageFooter}>
-      HEY
+    <div className={styles.pageFooterContainer}>
+      <h2>
+        Author: Guillem Senabre
+      </h2>
+      <h2>
+        hey2
+      </h2>
     </div>
   )
 }
